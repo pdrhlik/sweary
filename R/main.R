d795023c1b14d583b797a4bc177011e1680c0dce
Patrik Drhlík <patrioscraper@gmail.com>
2018-07-06 23:03:27 +0200

    create R package skeleton
    It has a completed DESCRIPTION file, NAMESPACE file generated by roxygen2
    and a simple main.R file with a dummy function called swear_words. It is
    the same function used here:
    https://github.com/pdrhlik/southparkr/blob/master/R/analyses.R.
    
    It just returns a vector of picked swear words.
    
    Proper software engineering design will be needed so that the package can
    actually be used in the future and that it can be easily usable and
    extensible.

---
+++ R/main.R
@@ -0,0 +1,10 @@
+#' List of english words that are considered naughty.
+#'
+#' @export
+swear_words <- function() {
+	return(c("shit", "fuck", "motherfuck", "cunt", "ass", "asshole", "cum",
+			 "gay", "dick", "dickhead", "piss", "douche", "douchebag", "twat",
+			 "bitch", "damn", "crap", "balls", "tit", "pussy", "slut", "fag",
+			 "fatass", "assholes", "gai", "shitti", "pussi", "damn", "goddamn",
+			 "bullshit", "bullcrap", "dumbass"))
+}
