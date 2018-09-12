35b932e8bc8b1b72001ee4ea2b0774a502d3ee5d
Patrik Drhlík <patrioscraper@gmail.com>
2018-09-12 16:39:43 +0200

    export raw data as a package data frame

---
+++ data-raw/swear-words.R
@@ -0,0 +1,18 @@
+library(purrr)
+library(readr)
+library(stringr)
+library(dplyr)
+
+load_lang <- function(lang_file) {
+	words <- readr::read_csv(lang_file, col_names = c("word"))
+	lang <- stringr::str_extract(lang_file, "\\w+$")
+
+	words$language <- lang
+
+	return(words)
+}
+
+lang_files <- list.files("data-raw/swear-word-lists/", full.names = TRUE)
+swear_words <- purrr::map_df(lang_files, load_lang)
+
+devtools::use_data(swear_words, overwrite = TRUE)
