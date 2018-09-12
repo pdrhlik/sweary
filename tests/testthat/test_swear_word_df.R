c94da2855f7a7e162ebcbfe54153f3f136e66852
Patrik Drhlík <patrioscraper@gmail.com>
2018-09-12 22:51:26 +0200

    add tests to check a swear_word data frame

---
+++ tests/testthat/test_swear_word_df.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(purrr)
+
+context("Swear word data frame")
+
+test_that("swear word data frame exists", {
+	expect_true(exists("swear_words"))
+})
+
+test_that("swear word df contains all raw data", {
+	n_lang_df <- length(unique(swear_words$language))
+	n_lang_files <- length(swear_word_files)
+
+	expect_equal(n_lang_df, n_lang_files)
+
+	n_words <- purrr::map_dbl(swear_word_files, function(f) {
+		length(readLines(f))
+	}) %>% sum()
+
+	expect_equal(n_words, nrow(swear_words))
+})
