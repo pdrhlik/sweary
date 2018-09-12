b03fc5edab84ba8a5ae91deecebecb29f220fada
Patrik Drhlík <patrioscraper@gmail.com>
2018-09-12 22:50:07 +0200

    create a helper test file for global test functions and variables

---
+++ tests/testthat/helper_file_names.R
@@ -0,0 +1,20 @@
+# Test variables
+swear_word_folder <- "../../data-raw/swear-word-lists"
+swear_word_files <- list.files(swear_word_folder, full.names = TRUE)
+
+#' Extracts file name from a file path.
+#'
+#' It splits a file path using a platform specific directory
+#' separator, unlists the results and returns the last element
+#' of the vector.
+#'
+#' @param file_path Character file path.
+#' @param ds Directory separator that defaults to a platform default.
+#'
+#' @return Character file name.
+file_name <- function(file_path, ds = .Platform$file.sep) {
+	ret <- unlist(strsplit(file_path, ds))
+	ret <- ret[length(ret)]
+
+	return(ret)
+}
