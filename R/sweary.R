920d79f0cf60f747491bd4169955c072693dc499
Patrik Drhlík <patrioscraper@gmail.com>
2018-09-14 13:21:00 +0200

    add data frame description

---
+++ R/sweary.R
@@ -0,0 +1,18 @@
+#' sweary: A database of swear words from multiple languages
+#'
+"_PACKAGE"
+
+#' Data frame of swear words
+#'
+#' A dataset containing a list of swear words across multiple
+#'   languages.
+#'
+#' \describe{
+#'   \item{word}{A swear word.}
+#'   \item{language}{A character code of the language the swear word
+#'     is recorded in.}
+#' }
+#'
+#' @docType data
+#' @format A data frame with 96 rows and 2 variables.
+"swear_words"
